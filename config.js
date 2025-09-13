df823431308be12229641c3d0afd40371e8a2a60
Dileep Krishna.M <dileepkrishna.madineni@gmail.com>
2025-09-13 19:36:38 +0530

    Removed config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const API_KEY= 'AIzaSyCf5beNgptqniV4gedR4UVgBmdoplEAudk';
-const AUTH_DOMAIN='lofd-6b3c0.firebaseapp.com';
-const PROJECT_ID='lofd-6b3c0';
-const STORAGE_BUCKET='lofd-6b3c0.firebasestorage.app';
-const MESSAGING_SENDER_ID='87506444207';
-const APP_ID='1:87506444207:web:7ce5bba90c412bddca995a';
-export default {API_KEY,AUTH_DOMAIN,PROJECT_ID,STORAGE_BUCKET,MESSAGING_SENDER_ID,APP_ID};
